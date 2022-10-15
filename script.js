e6619b61b37a126624138a0b0e44869af051faaa
codeSB10 <susan.baral1708@gmail.com>
2022-10-16 01:31:56 +0545

    2nd commit: Added Loading stage

--- script.js
+++ script.js
@@ -12,44 +12,12 @@ function closeMenu() {
     document.body.classList.remove("menu--open");
 }
 
-// function showLoad() {
-//     document.querySelector('.search__bar input').addEventListener('change', loading);
-//     document.querySelector('.fa-magnifying-glass').addEventListener('click', loading);
-// }
-
-// function hideLoad() {
-//     document.querySelector('.search__bar input').removeEventListener('change', loading);
-//     document.querySelector('.fa-magnifying-glass').removeEventListener('click', loading);
-// }
-
-// function loading() {
-//     return resultsElem.innerHTML = `
-//         <div class ="loading">
-//             <i class="fa-solid fa-spinner"></i>
-//         </div>
-//     `
-// }
-
-const resultsElem = document.querySelector('#results');
-
-async function searchMovie(event) {
-    const searched = event.target.value;
-    if (!!searched) {
-        const response = await fetch(`http://www.omdbapi.com/?apikey=6f07cddd&s=${searched}`);        
-        const data = await response.json();
-        if (data.Response !== 'False') {
-            const movies = data.Search.slice(0, 6);
-            return resultsElem.innerHTML = movies.map(value => movie(value)).join("");
-        }
-        else {
-            return resultsElem.innerHTML = '<h3 class="msg">Error. Search something else. !!</h3>'
-        }
-    }
-    return resultsElem.innerHTML = '<h3 class="msg">Search something to show results !!</h3>'
-}
-
-function searchMovie2() {
-    return searchMovie();
+function loading() {
+    return resultsElem.innerHTML = `
+        <div class ="loading">
+            <i class="fa-solid fa-spinner"></i>
+        </div>
+    `
 }
 
 function movie(value) {
@@ -62,4 +30,27 @@ function movie(value) {
             </div>
         </div>
     `
+}
+
+const resultsElem = document.querySelector('#results');
+
+async function searchMovie(event) {
+    const searched = event.target.value;
+    if (!!searched) {
+        loading();
+        const response = await fetch(`http://www.omdbapi.com/?apikey=6f07cddd&s=${searched}`);
+        const data = await response.json();
+        setTimeout(() => {
+            if (data.Response !== 'False') {
+                const movies = data.Search.slice(0, 6);
+                return resultsElem.innerHTML = movies.map(value => movie(value)).join("");
+            }
+            else {
+                return resultsElem.innerHTML = '<h3 class="msg">Error. Search something else. !!</h3>'
+            }
+        }, 1000);
+    }
+    else {
+        return resultsElem.innerHTML = '<h3 class="msg">Search something to show results !!</h3>'
+    }
 }
